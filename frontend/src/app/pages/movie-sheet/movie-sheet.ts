c3a17e3c00887e4640e236549710a3da8d9269cb
📺 IanF 📺 <ianfrancano2006@gmail.com>
2025-10-21 20:10:58 -0300

    Implementing the project
    This commit uploads the entire SpringBoot project with basic configurations to work alongside Angular, as well as the creation and separation of the project parts (front, docs, and back).

---
+++ frontend/src/app/pages/movie-sheet/movie-sheet.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-sheet',
+  imports: [],
+  templateUrl: './movie-sheet.html',
+  styleUrl: './movie-sheet.css'
+})
+export class MovieSheet {
+
+}
