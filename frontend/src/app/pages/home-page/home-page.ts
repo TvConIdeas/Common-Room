aa58586b370033907bd4cb44b590c76047d1228e
📺 IanF 📺 <ianfrancano2006@gmail.com>
2025-11-07 13:31:31 -0300

    Add movie models and service integration to frontend
    Introduced MovieBase and MovieDetails models for type safety. Updated MovieService to fetch movies from backend API, including recent, popular, and upcoming movies. Enhanced HomePage to display recent movies and fetch data via MovieService. Enabled HttpClient in app config. Updated backend datasource password for local development.

--- frontend/src/app/pages/home-page/home-page.ts
+++ frontend/src/app/pages/home-page/home-page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import MovieBase from '../../models/MovieBase';
+import { MovieService } from '../../services/movie-service';
 
 @Component({
   selector: 'app-home-page',
@@ -6,6 +8,31 @@ import { Component } from '@angular/core';
   templateUrl: './home-page.html',
   styleUrl: './home-page.css'
 })
-export class HomePage {
+export class HomePage implements OnInit{
+
+  /* ---- Array de peliculas a mostrar ---- */
+  recentMovies : MovieBase[] = [];
+  popularMovies : MovieBase[] = [];
+  upcomingMovies : MovieBase[] = [];
+
+  /* -- Pagina actual de cada Lista -- */
+  currentPageRecent = 1;
+  currentPagePopular = 1;
+  currentPageUpcoming = 1;
+
+  constructor(private mService : MovieService) {}
+
+  ngOnInit(): void {
+    this.loadRecentMovies();
+  }
+
+  /* -------- Funcion para cargar las peliculas actuales -------- */
+  loadRecentMovies() : void {
+    this.mService.getRecentMovies(this.currentPage).subscribe({
+      next : (data) => this.recentMovies = data,
+      error : (e) => console.error(e)
+    })
+  }
+
 
 }
